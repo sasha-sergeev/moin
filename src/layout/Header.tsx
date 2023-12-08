b96d257af8f48599308b657200670451ca3a1288
Sasha Sergeev <sasha.sergeev@tngtech.com>
2023-12-08 22:14:05 +0100

    header has padding

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -11,22 +11,25 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
     return (
-        <AppBar position="fixed">
-            <Toolbar>
-                <IconButton
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                    onClick={toggleSidebar}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Moin!
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                        onClick={toggleSidebar}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Moin!
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Toolbar/>
+        </>
     )
 }
 
